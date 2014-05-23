5cdc781c570a51bba70f76e108e00db171a8662b
Manu Sporny <msporny@digitalbazaar.com>
2014-05-23 01:19:30 -0400

    Remove pure HTML-based site for login.dev (POSTs are needed).

--- sites/credentials.dev/credentials.php
+++ sites/credentials.dev/credentials.php
@@ -1,3 +1,17 @@
+<?php
+include 'utils.php';
+session_start();
+
+$credentials = false;
+
+// attempt a login if login information was POSTed
+if(!empty($_POST)) {
+  // FIXME: implement login
+  $credentials = json_encode(json_decode($_POST['response'], true),
+    JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,7 +21,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Mixnet Login</title>
+    <title>Credential Issuer</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
@@ -16,7 +30,7 @@
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
     <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
+    <link href="cover.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -28,18 +42,34 @@
     <![endif]-->
   </head>
 
-  <body onload="initTelehash();">
+  <body>
 
-    <div class="container">
+    <div class="site-wrapper">
 
-      <form class="form-signin" role="form" onsubmit="login();">
-        <h2 class="form-signin-heading">Mixnet Login</h2>
-        <input id="email" type="email" class="form-control" placeholder="Email address" required autofocus value="msporny@digitalbazaar.com">
-        <input id="passphrase" type="password" class="form-control" placeholder="Passphrase" required value="zasswordyasswordxasswordwassword">
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-      </form>
+      <div class="site-wrapper-inner">
 
-    </div> <!-- /container -->
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+
+            <h2 class="form-signin-heading">Retrieved Credentials</h2>
+            <pre style="text-align: left;"><?php echo $credentials; ?></pre>
+
+            <?php if($error) echo '<div class="alert alert-danger">'.$error_message.'</div>' ?>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -47,10 +77,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-    <script src="async.min.js"></script>
-    <script src="scrypt.min.js"></script>
-    <script src="forge.min.js"></script>
-    <script src="telehash.min.js"></script>
-    <script src="login.js"></script>
   </body>
 </html>
